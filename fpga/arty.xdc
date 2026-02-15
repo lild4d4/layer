db55e8118f18477a8da3fd1fd3ca83972297a33a
janschaible <janphilip.schaible@gmail.com>
2026-02-15 11:26:57 +0100

    before vivado

--- fpga/arty.xdc
+++ fpga/arty.xdc
@@ -10,6 +10,10 @@ set_property IOSTANDARD LVCMOS33 [get_ports {clk}]
 #set_property -dict { PACKAGE_PIN F1    IOSTANDARD LVCMOS33 } [get_ports { sclk }]; #IO_L18P_T2_35 Sch=ck_sck
 #set_property -dict { PACKAGE_PIN C1    IOSTANDARD LVCMOS33 } [get_ports { ss }]; #IO_L16N_T2_35 Sch=ck_ss
 
+set_property -dict { PACKAGE_PIN V15   IOSTANDARD LVCMOS33 } [get_ports { ss  }]; #IO_L16P_T2_CSI_B_14 Sch=ck_io[0]
+set_property -dict { PACKAGE_PIN U16   IOSTANDARD LVCMOS33 } [get_ports { miso  }]; #IO_L18P_T2_A12_D28_14 Sch=ck_io[1]
+set_property -dict { PACKAGE_PIN P14   IOSTANDARD LVCMOS33 } [get_ports { mosi  }]; #IO_L8N_T1_D12_14 Sch=ck_io[2]
+set_property -dict { PACKAGE_PIN T11   IOSTANDARD LVCMOS33 } [get_ports { sclk  }]; #IO_L19P_T3_A10_D26_14 Sch=ck_io[3]
 
 ## Switches
 set_property -dict { PACKAGE_PIN A8    IOSTANDARD LVCMOS33 } [get_ports { rst }]; #IO_L12N_T1_MRCC_16 Sch=sw[0]
--- layr/layr/SPI/test/test_spi/spi_top.sv
+++ layr/layr/SPI/test/test_spi/spi_top.sv
@@ -1,32 +1,60 @@
 module spi_top(
     input         clk,
-    input         rst,
+    (* MARK_DEBUG = "TRUE" *) input         rst,
 
-    
-    output wire [3:0] led,
+    output logic [3:0] led,
 
     // SPI bus
-    output wire       sclk,
-    output wire       mosi,
+    (* MARK_DEBUG = "TRUE" *) output wire       sclk,
+    (* MARK_DEBUG = "TRUE" *) output wire       mosi,
     input  wire       miso,
     output reg        ss
 );
 
-spi_echo echo (
+logic go, done, busy;
+logic spi_clk;
+reg [23:0] ctr; // 24 bits to count up to 10,000,000
+
+clock_divider divider(
     .clk(clk),
     .rst(rst),
-
-    // Control interface
-    .go(~rst),
-    .tx_byte(8'h2),
-
-    .sclk(sclk),
-    .mosi(mosi),
-    .miso(miso),
-    .ss(ss)
+    .clk_out(spi_clk)
 );
 
-assign led[0] = rst;
+spi_master u_spi (
+    .clk     (spi_clk),
+    .reset   (rst),
+    .data_in (8'hbe),
+    .start   (go),
+    .miso    (miso),
+    .mosi    (mosi),
+    .sclk    (sclk),
 
+    .busy(busy)
+);
+
+always_ff @(posedge spi_clk or posedge rst) begin
+    if (rst) begin
+        ctr <= 1;
+        go <= 0;
+        led <= 4'b0000;
+        ss <= 0;
+    end else begin
+        if (ctr == 0) begin
+            if(~busy)begin
+                ctr <= 9_999_999;       // reset counter every 1 second
+                go <= 1;        // pulse `go` for 1 clock cycle
+                led[0] <= ~led[0]; // toggle only led[0]
+            end
+        end else begin
+            ctr <= ctr - 1;
+            go <= 0;
+        end
+    end
+end
 
 endmodule
+
+
+
+
